2b58077f22a0834dc6e73f02b1503f7767730da3
Lukas Rose <public@lrose.de>
2016-06-02 00:28:11 +0200

    Menue4 (Speichern an Datei) fertiggestellt

--- Clog/src/menues/Menue4.java
+++ Clog/src/menues/Menue4.java
@@ -1,8 +1,16 @@
 package menues;
 
+import data.DatensatzManager;
+import eingabeausgabe.Ausgabe;
+import eingabeausgabe.Eingabe;
+import eingabeausgabe.Path;
+
 public class Menue4 {
 	public static void menue(){
-		
+		Ausgabe.printline("Bitte geben Sie einen Dateipfad ein, an den die Datei gespeichert werden soll.");
+		Path path = new Path(Eingabe.naechsterString());
+		DatensatzManager.saveToFile(path);
+		Ausgabe.printline("Gespeichert!");
 	}
 
 }
