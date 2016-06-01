ed5b81cc2e4261db8af3f1503f29f7b86bcbbf4b
Lukas Rose <public@lrose.de>
2016-06-02 00:25:58 +0200

    Finished menu to read from files (Menue3)

--- Clog/src/menues/Menue3.java
+++ Clog/src/menues/Menue3.java
@@ -1,12 +1,15 @@
 package menues;
 
+import data.DatensatzManager;
 import eingabeausgabe.Ausgabe;
 import eingabeausgabe.Eingabe;
+import eingabeausgabe.Path;
 
 public class Menue3 {
 	public static void menue(){
 		Ausgabe.printline("Bitte geben Sie einen Dateipfad ein, von dem die Datei geladen werden soll.");
 		Path path = new Path(Eingabe.naechsterString());
-		V
+		DatensatzManager.readFromFile(path);
+		Ausgabe.printline("Eingelesen!");
 	}
 }
