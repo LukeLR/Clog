d221d9019bc04017c9839290e92a2cf8ff0d74c7
Lukas Rose <public@lrose.de>
2016-06-02 00:15:18 +0200

    Implemented searching for Entries (Menue2)

--- Clog/src/menues/Menue2.java
+++ Clog/src/menues/Menue2.java
@@ -1,7 +1,14 @@
 package menues;
 
+import data.DatensatzListe;
+import eingabeausgabe.Ausgabe;
+import eingabeausgabe.Eingabe;
+import textangaben.Schlagwort;
+
 public class Menue2 {
 	public static void menue(){
-		
+		Ausgabe.print("Bitte geben Sie ein Schlagwort ein. Es werden alle Einträge mit diesem Schlagwort ausgeben.");
+		Schlagwort schlagwort = new Schlagwort(Eingabe.naechsterString());
+		DatensatzListe.datensaetzeMitSchlagwortAusgeben(schlagwort);
 	}
 }
